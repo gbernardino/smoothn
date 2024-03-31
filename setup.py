85071ef686d1b98f9e3d9293a7f2ac5a65018be3
Gabriel Bernardino <gabriel.bernardino1@gmail.com>
2024-03-31 18:21:52 +0200

    Reorganised for make it pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools 
+
+setuptools.setup(
+    name = "smoothn",
+    version = "0.0.1",
+    author = "Santiago Parraguez",
+    description = "Python implementation of the smoothn function originally developed by D. Garcia in Matlab",
+    url = "https://github.com/SanParraguez/smoothn",
+    packages = ["smoothn"],
+)
